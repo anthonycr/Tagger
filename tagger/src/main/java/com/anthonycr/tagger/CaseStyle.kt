cbb014727897c61723c0e6a86f89b814a5cc40bd
Anthony Restaino <anthonyrestaino11@gmail.com>
2017-12-05 22:01:48 -0500

    Documentation for annotation

--- tagger/src/main/java/com/anthonycr/tagger/CaseStyle.kt
+++ tagger/src/main/java/com/anthonycr/tagger/CaseStyle.kt
@@ -1,7 +1,9 @@
 package com.anthonycr.tagger
 
 /**
- * Created by anthonycr on 12/4/17.
+ * The capitalization style that the tag should use.
+ * - [ALL_CAPS]: The class name will be forced to uppercase.
+ * - [NORMAL]: The class name will be used unchanged.
  */
 enum class CaseStyle {
     ALL_CAPS,
--- tagger/src/main/java/com/anthonycr/tagger/Tag.kt
+++ tagger/src/main/java/com/anthonycr/tagger/Tag.kt
@@ -2,6 +2,9 @@ package com.anthonycr.tagger
 
 /**
  * An annotation to tag classes which need a tag generated.
+ *
+ * @param caseStyle the capitalization style that should be used when generating the tag.
+ * @see CaseStyle
  */
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.CLASS)
