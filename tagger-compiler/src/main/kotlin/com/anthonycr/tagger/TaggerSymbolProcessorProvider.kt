d8130f7e3968ad2de3257c46cff88dd87d90030f
Anthony Restaino <anthonyrestaino11@gmail.com>
2025-03-06 20:57:18 -0500

    Add documentation for the provider

--- tagger-compiler/src/main/kotlin/com/anthonycr/tagger/TaggerSymbolProcessorProvider.kt
+++ tagger-compiler/src/main/kotlin/com/anthonycr/tagger/TaggerSymbolProcessorProvider.kt
@@ -4,6 +4,10 @@ import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
+/**
+ * This provider abstracts away the creation of the [SymbolProcessor] allowing us to do proper
+ * dependency injection.
+ */
 class TaggerSymbolProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor =
         TaggerSymbolProcessor(
